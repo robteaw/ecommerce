eeaef73786e274ad5f572b10ee8a59ea11615ae3
Robert Tea <robteaw@gmail.com>
2020-09-25 22:21:57 -0500

    check cart models

--- carts/models.py
+++ carts/models.py
@@ -10,10 +10,10 @@ class CartManager(models.Manager):
     def new_or_get(self, request):
         cart_id = request.session.get("cart_id", None)
         qs = self.get_queryset().filter(id=cart_id)
-        if qs.count() == 1: 
+        if qs.count() == 1:
             new_obj = False
             cart_obj = qs.first()
-            if request.user.is_authenticated and cart_obj.user is None:
+            if request.user.is_authenticated() and cart_obj.user is None:
                 cart_obj.user = request.user
                 cart_obj.save()
         else:
@@ -25,7 +25,7 @@ class CartManager(models.Manager):
     def new(self, user=None):
         user_obj = None
         if user is not None:
-            if user.is_authenticated:
+            if user.is_authenticated():
                 user_obj = user
         return self.model.objects.create(user=user_obj)
 
@@ -50,12 +50,13 @@ def m2m_changed_cart_receiver(sender, instance, action, *args, **kwargs):
         for x in products:
             total += x.price
         if instance.subtotal != total:
-            instance.total = total
+            instance.subtotal = total
             instance.save()
 
 m2m_changed.connect(m2m_changed_cart_receiver, sender=Cart.products.through)
 
+
 def pre_save_cart_receiver(sender, instance, *args, **kwargs):
-    instance.total = instance.subtotal + 10 # * 1.08
+    instance.total = instance.subtotal  + 10 #* 1.08
 
 pre_save.connect(pre_save_cart_receiver, sender=Cart)
